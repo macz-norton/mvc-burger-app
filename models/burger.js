ea2a72621354adbf614470b036bd1eee68d36cd6
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-27 20:14:25 -0800

    Added the models for all, create, and update ffor the ORM to interact with the database

--- models/burger.js
+++ models/burger.js
@@ -3,13 +3,38 @@ const orm = require("../config/orm");
 const burger = {
 
     // all
+    all: (callback) => {
+
+        orm.all("burgers", (res) => {
+
+            callback(res);
+
+        })
+
+    },
 
     // create
+    create: (cols, vals, callback) => {
+
+        orm.create("burgers", cols, vals, (res) => {
+
+            callback(res);
+
+        });
+
+    },
 
     // update
+    update: (objColVals, condition, callback) {
 
+        orm.update("burgers", objColVals, condition, (res) => {
+
+            callback(res);
+
+        });
+
+    }
 
 }
-
 
 module.exports = burger;
