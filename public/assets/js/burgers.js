b8899c203d995fd738cf4ef841fd7ea7c076fd09
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-28 08:55:43 -0800

    Added JavaScript for on click for adding burger and devouring burger

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,43 @@
+$(function() {
+  $(".change-devoured").on("click", (event) => {
+    const id = $(this).data("id");
+    const newDevoured = $(this).data("newDevoured");
+
+    const newDevouredState = {
+      devoured: newDevoured
+    };
+
+    $.ajax("/api/burgeres/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(() => {
+      console.log("Changed devoured to," newDevoured);
+      location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", (event) => {
+
+    event.preventDefault();
+
+    const newBurger = {
+      name: $("#burger").val().trim()
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newDevoured
+    }).then(() => {
+      console.log("Created a new burger");
+      location.reload();
+      }
+    );
+  });
+});
+
 // Animated letters
-var textWrapper = document.querySelector('.ml6 .letters');
+const textWrapper = document.querySelector('.ml6 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
