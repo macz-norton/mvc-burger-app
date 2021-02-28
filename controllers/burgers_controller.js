9a5ed1b6a3ed235abab8f967cc20191eb713104a
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-27 20:22:32 -0800

    Updated all function in controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,18 @@ const router = express.Router();
 // all
 router.get("/", (req, res) => {
 
-})
+    burger.all((data) => {
+
+        const handlebarsObject = {
+            burgers: data
+        };
+
+        console.log(handlebarsObject);
+        res.render("index", handlebarsObject);
+
+    });
+    
+});
 
 // create
 router.post("/api/burgers", (req, res) => {
