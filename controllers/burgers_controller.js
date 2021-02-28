edda9328ff4a8ecf57b5423d0ef2284911b51850
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-27 20:20:09 -0800

    Added structure for the different routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const burger = require("../models/burger");
 
+const router = express.Router();
+
+// all
+router.get("/", (req, res) => {
+
+})
+
+// create
+router.post("/api/burgers", (req, res) => {
+
+})
+
+// update
+router.put("/api/burgers/:id", (req, res) => {
+    
+})
+
+
 module.exports = router;
