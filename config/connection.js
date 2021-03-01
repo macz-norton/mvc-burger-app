03b96c920f60c664ffa78d9eea459e216a1f4973
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-03-01 08:33:56 -0800

    Updated index.handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
 
-let connection = mysql.createConnection(process.env.JAWSDB_URL) || mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "2g7pXzH&oA5S",
-    database: "burgers_db"
-})
+let connection;
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASS,
+        database: "burgers_db"
+    })
+} 
 connection.connect(err => {
 
     if (err) {
